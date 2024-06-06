e0cd1b01322ace2b81a7c032fc317d0996a44ffd
giliyablo <gili.yablonka@gmail.com>
2024-06-06 15:25:29 +0300

    e2e -> 18.153.72.201:80

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   
   e2e: {
-    'baseUrl': 'http://localhost:80'
+    'baseUrl': 'http://18.153.72.201:80'
   },
   
   
--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,6 +1,6 @@
 describe('My First Test', () => {
   it('Visits the initial project page', () => {
-    cy.visit('http://localhost:80/')
+    cy.visit('http://18.153.72.201:80/')
     cy.contains('app is running!')
   })
 })
