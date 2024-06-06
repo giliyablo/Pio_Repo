6591b67e89c25cd7edb7998eb9ba5654dbb74a18
giliyablo <gili.yablonka@gmail.com>
2024-06-06 12:56:11 +0300

    e2e

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,6 +1,6 @@
 describe('My First Test', () => {
   it('Visits the initial project page', () => {
-    cy.visit('/')
+    cy.visit('http://18.153.72.201/')
     cy.contains('app is running!')
   })
 })
